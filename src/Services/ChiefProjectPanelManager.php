6d29b0a5147cfe87f6c9f5508f9d207b08bba754
Jaime <jaimeruizperez@gmail.com>
2020-10-12 19:49:29 +0200

    Esqueletos servicios de paneles del técnico y del comercial

--- src/Services/ChiefProjectPanelManager.php
+++ src/Services/ChiefProjectPanelManager.php
@@ -10,30 +10,51 @@ class ChiefProjectManager {
     }
     
     /**
-     * Ver listado de solicitudes de presupuesto  
+     * Mostrar la lista de proyectos en marcha  
      */
-    public function viewBudgetRequestList() : void {
+    public function viewWorkingProjetList() :void {
         
     }
     
     /**
-     * Modificar una solicitud de presupuesto  
+     * Mostrar el histórico de proyectos  
      */
-    public function editBudgetRequest(BudgetRequest $budgetRequest) : void {
+    public function viewProjectHistory() : void {
         
     }
     
     /**
-     * Rechazar una solicitud de presupuesto  
-     */
-    public function rejectBudgetRequest(BudgetRequest $budgetRequest) : void {
+     * Ascociar técnico a un proyecto en marcha
+    */
+    public function addTechniciamToWorkingProject(User $technician, Project $project1) : void {
         
     }
     
     /**
-     * Aceptar una solicitud de presupuesto  
-     */
-    public function acceptBudgetRequest(BudgetRequest $budgetRequest) : void {
+     * Añadir tarea a un proyecto
+    */
+    public function createTaskInProject(Task $task, Project $project) : void {
+        
+    }
+    
+    /**
+     * Añadir un técnico a una tarea del proyecto
+    */
+    public function addTechnicianToTask(User $technician, Task $task, Project $project) : void {
+        
+    }
+    
+    /**
+     * Intercambiar técnicos en una tarea
+    */
+    public function swapTechnicianInTask(User $formerTechnician, User $newTechnician, Task $task) : void {
+        
+    }
+    
+    /**
+     * Camiar el estado del proyecto
+    */
+    public function changeProjectStatus(Status $status, Project $project) {
         
     }
 }
--- src/Services/SalesPanelManager.php
+++ src/Services/SalesPanelManager.php
@@ -5,5 +5,43 @@
  *
  */
 class SalesPanelManager {
-    //put your code here
+    
+    public function __contruct() {
+        
+    }
+    
+    /**
+     * Ver listado de solicitudes de presupuesto  
+     */
+    public function viewBudgetRequestList() : void {
+        
+    }
+    
+    /**
+     * Modificar una solicitud de presupuesto  
+     */
+    public function editBudgetRequest(BudgetRequest $budgetRequest) : void {
+        
+    }
+    
+    /**
+     * Rechazar una solicitud de presupuesto  
+     */
+    public function rejectBudgetRequest(BudgetRequest $budgetRequest) : void {
+        
+    }
+    
+    /**
+     * Aceptar una solicitud de presupuesto  
+     */
+    public function acceptBudgetRequest(BudgetRequest $budgetRequest) : void {
+        
+    }
+    
+    /**
+     * Mostrar los datos de un proyecto  
+     */
+    public function viewProjectInfo(Project $project) {
+        
+    }
 }
