9cbabcef53b9ea90173ea0476f8efcb987599c24
Jaime <jaimeruizperez@gmail.com>
2020-10-12 20:06:41 +0200

    Esqueleto servicio del panel del técnico

--- src/Services/TechnicianPanelManager.php
+++ src/Services/TechnicianPanelManager.php
@@ -5,5 +5,36 @@
  *
  */
 class TechnicianPanelManager {
-    //put your code here
+    
+    public function __contruct() {
+        
+    }
+    
+    /**
+     * Ver información del proyecto  
+     */
+    public function viewProjectInfo(Project $project) : void {
+        
+    }
+    
+    /**
+     * Ver listado de tareas de un proyecto  
+     */
+    public function viewProjectTaskList(Project $project) : void {
+        
+    }
+    
+    /**
+     * Marcar tarea como finalizada  
+     */
+    public function setTaskAsFinished(Task $task) : void {
+        
+    }
+    
+    /**
+     * Ver el grado de avance de un proyecto
+     */
+    public function viewProjectTimeline(Project $project) : void {
+        
+    }
 }
