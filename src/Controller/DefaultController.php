f1b2cbcbdb08633d72b5dcbfe54779bf3f5f0c11
Jaime <jaimeruizperez@gmail.com>
2020-10-24 16:26:28 +0200

    Ajuste DefaultController

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -8,8 +8,6 @@ class DefaultController
 {
     public function index(): Response
     {
-        $number = random_int(0, 100);
-
         return new Response(
             '<html><body>Default page</body></html>'
         );
