65dc4fe0014ab2c79ee8e9c2e7d328fcf074651c
Jaime <jaimeruizperez@gmail.com>
2020-11-08 17:51:02 +0100

    Añadido Gaurdsuscriber para comprobar tareas completadas de un proyecto

---
+++ src/EventSuscriber/ProjectSuscriber.php
@@ -0,0 +1,24 @@
+<?php
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Workflow\Event\GuardEvent;
+
+class ProjectSuscriber implements EventSubscriberInterface {
+    
+    public static function getSubscribedEvents()
+    {
+        return array('workflow.manage_project.to_finished' => 'checkCompletedTasks',);
+    }
+    
+    public function checkCompletedTasks(GuardEvent $event)
+    {
+        $project = $event->getSubject();
+        $task_list = $project->getTasks();
+        foreach($task_list as $task) {
+            if($task->getState() != 'finished') {
+                $event->setBlocked(true, 'No ha podido realizarse la acción porque quedan tareas por terminar');
+                break;
+            }
+        }
+    }
+}
